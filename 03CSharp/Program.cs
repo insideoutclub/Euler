20a7effdf80c978409f2ef34ece7778ca4c2d8ae
unknown <sandersd@LIZZY.ter.teradyne.com>
2014-09-26 10:56:53 -0700

    Working on Problem 3.

--- 03CSharp/Program.cs
+++ 03CSharp/Program.cs
@@ -2,11 +2,28 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Diagnostics;
+using Function = System.Func<long, long>;
 
 namespace _03CSharp
 {
     class Program
     {
+        // Run action the number of times specified by iterations
+        // Returns the elapsed time in milliseconds
+        public static double Benchmark(Action action, int iterations)
+        {
+            action();
+
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+
+            var stopwatch = Stopwatch.StartNew();
+            for (var i = 0; i != iterations; ++i)
+                action();
+            return stopwatch.Elapsed.TotalMilliseconds;
+        }
+
         static IEnumerable<int> divisors()
         {
             yield return 2;
@@ -17,35 +34,88 @@ namespace _03CSharp
                 yield return result += addend;
         }
 
-        static IEnumerable<int> primeFactors(int n)
+        struct QuotientAndRemainder
         {
-            var d = divisors().GetEnumerator();
-            d.MoveNext();
+            public long quotient;
+            public long remainder;
+        }
 
-            A2:
-            if (n == 1) yield break;
-            int r;
+        static QuotientAndRemainder quotientAndRemainder(long dividend, long divisor)
+        {
+            QuotientAndRemainder result;
+            result.quotient = Math.DivRem(dividend, divisor, out result.remainder);
+            return result;
+        }
 
-            A3:
-            var q = Math.DivRem(n, d.Current, out r);
-            if(r != 0)
-                goto A6;
-            yield return d.Current;
-            n = q;
-            goto A2;
-
-            A6:
-            if(q > d.Current) {
-                d.MoveNext();
-                goto A3;
+        static long largestPrimeFactor(long n)
+        {
+            var divisor = 2;
+            var result = 1;
+            while(n > 1)
+            {
+                var q = quotientAndRemainder(n, divisor);
+                if(q.remainder == 0)
+                {
+                    result = divisor;
+                    n = q.quotient;
+                    while((q = quotientAndRemainder(n, divisor)).remainder == 0)
+                        n = q.quotient;
+                }
+                ++divisor;
             }
+            return result;
+        }
 
-            yield return n;
+        static long largestPrimeFactor2(long n)
+        {
+            var divisor = 2;
+            var result = 1;
+            while (n > 1)
+            {
+                var q = quotientAndRemainder(n, divisor);
+                if (q.remainder == 0)
+                {
+                    result = divisor;
+                    n = q.quotient;
+                    while ((q = quotientAndRemainder(n, divisor)).remainder == 0)
+                        n = q.quotient;
+                }
+                divisor += divisor == 2 ? 1 : 2;
+            }
+            return result;
+        }
+
+        static long largestPrimeFactor3(long n)
+        {
+            var divisor = 2;
+            var result = 1;
+            while (n > 1)
+            {
+                var q = new QuotientAndRemainder();
+                while((q = quotientAndRemainder(n, divisor)).remainder == 0)
+                {
+                    result = divisor;
+                    n = q.quotient;
+                }
+                divisor += divisor == 2 ? 1 : 2;
+            }
+            return result;
         }
 
         static void Main(string[] args)
         {
-            Console.WriteLine(string.Join(", ", primeFactors(13195)));
+            var experiments = new Function[] {
+                largestPrimeFactor,
+                largestPrimeFactor2,
+                largestPrimeFactor3,
+            };
+
+            const long n = 600851475143;
+
+            foreach (var result in experiments
+                .Select(func => new { result = func(n), time = Benchmark(() => func(n), 1000), name = func.Method.Name })
+                .OrderBy(tuple => tuple.time))
+                Console.WriteLine("{0,-30} {1} {2:0.0000}", result.name, result.result, result.time);
         }
     }
 }
