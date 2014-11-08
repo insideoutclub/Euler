ef4a93a6387300f8fdb4a46d13ba7940efc0c0fc
unknown <sandersd@LIZZY.ter.teradyne.com>
2014-11-07 16:13:03 -0800

    Final changes in preparation for first presentation.

--- 01CPlusPlus/Program.cpp
+++ 01CPlusPlus/Program.cpp
@@ -8,22 +8,9 @@
 #include <iomanip>
 #include <vector>
 
-// Returns true if dividend is a multiple of divisor
-struct IsMultipleOf {
-    explicit IsMultipleOf(int const divisor = int()) : divisor(divisor) {}
-    bool operator()(int const dividend) const { return dividend % divisor == 0; }
-    int const divisor;
-};
-
-// Returns true if dividend is a multiple of either divisor0 or divisor1
-struct IsMultipleOfEither {
-    explicit IsMultipleOfEither(int const divisor0 = int(), int const divisor1 = int())
-        : isMultipleOfDivisor0(divisor0), isMultipleOfDivisor1(divisor1) {}
-    bool operator()(int const dividend) const {
-        return isMultipleOfDivisor0(dividend) || isMultipleOfDivisor1(dividend);
-    }
-    IsMultipleOf isMultipleOfDivisor0;
-    IsMultipleOf isMultipleOfDivisor1;
+// Returns true if i is a multiple of 3 or 5
+struct IsMultipleOf3Or5 {
+    bool operator()(int const i) const { return i % 3 == 0 || i % 5 == 0; }
 };
 
 // Run action the number of times specified by iterations
@@ -46,68 +33,67 @@ static double Benchmark(Action action, int iterations)
   return double(finish.QuadPart - start.QuadPart) * 1000 / frequency.QuadPart;
 }
 
-// Generate [1, limit)
-// Filter out multiples of x and y
+// Generate [1, 1000)
+// Filter out multiples of 3 and 5
 // Returns sum
-static int FilterImperative(int limit, int x, int y)
+static int FilterImperative()
 {
   auto sum = 0;
-  auto const isMultipleOfXOrY = IsMultipleOfEither(x, y);
-  for (auto i = 1; i < limit; ++i)
-    if (isMultipleOfXOrY(i))
+  for (auto i = 1; i < 1000; ++i)
+    if (IsMultipleOf3Or5()(i))
       sum += i;
   return sum;
 }
 
-// Sum multiples of x
-// Sum multiples of y
-// Subtract multiples of x * y to eliminate duplicates
-static int GenerateMultiplesImperative(int limit, int x, int y)
+// Sum multiples of 3
+// Sum multiples of 5
+// Subtract multiples of 3 * 5 to eliminate duplicates
+static int GenerateMultiplesImperative()
 {
   auto sum = 0;
-  for (auto i = x; i < limit; i += x) sum += i;
-  for (auto i = y; i < limit; i += y) sum += i;
-  for (auto i = x * y; i < limit; i += x * y) sum -= i;
+  for (auto i = 3; i < 1000; i += 3) sum += i;
+  for (auto i = 5; i < 1000; i += 5) sum += i;
+  for (auto i = 3 * 5; i < 1000; i += 3 * 5) sum -= i;
   return sum;
 }
 
-// Compute the sum of [x, limit) step x using triangular numbers
-static int SumOfMultiplesOf(int x, int limit)
+// Compute the sum of [x, 1000) step x using triangular numbers
+static int SumOfMultiplesOf(int x)
 {
-  auto p = (limit - 1) / x;
+  auto p = (1000 - 1) / x;
   return x * p * (p + 1) / 2;
 }
 
-// Sum multiples of x
-// Sum multiples of y
-// Subtract multiples of x * y to eliminate duplicates
-static int SumOfMultiples(int limit, int x, int y)
+// Sum multiples of 3
+// Sum multiples of 5
+// Subtract multiples of 3 * 5 to eliminate duplicates
+static int SumOfMultiples()
 {
-  return SumOfMultiplesOf(x, limit) + SumOfMultiplesOf(y, limit) - SumOfMultiplesOf(x * y, limit);
+  return SumOfMultiplesOf(3) + SumOfMultiplesOf(5) - SumOfMultiplesOf(3 * 5);
 }
 
-// Generate [1, limit)
-// Filter out multiples of x and y
+// Generate [1, 1000)
+// Filter out multiples of 3 and 5
 // Returns sum
-static int FilterFunctional(int limit, int x, int y)
+static int FilterFunctional()
 {
-  return boost::accumulate(boost::irange(1, limit) | boost::adaptors::filtered(IsMultipleOfEither(x, y)), 0);
+  return boost::accumulate(boost::irange(1, 1000) | boost::adaptors::filtered(IsMultipleOf3Or5()), 0);
 }
 
-// Generate multiples of x up to but not including limit
-static boost::strided_integer_range<int> MultiplesOf(int x, int limit)
+// Generate multiples of x up to but not including 1000
+static boost::strided_integer_range<int> MultiplesOf(int x)
 {
-  return boost::irange(x, limit, x);
+  return boost::irange(x, 1000, x);
 }
 
-// Sum multiples of x
-// Sum multiples of y
-// Subtract multiples of x * y to eliminate duplicates
-static int GenerateMultiplesFunctional(int limit, int x, int y)
+// Sum multiples of 3
+// Sum multiples of 5
+// Subtract multiples of 3 * 5 to eliminate duplicates
+static int GenerateMultiplesFunctional()
 {
-  return boost::accumulate(MultiplesOf(x, limit), 0) +
-         boost::accumulate(MultiplesOf(y, limit), 0) -
-         boost::accumulate(MultiplesOf(x * y, limit), 0);
+  return boost::accumulate(MultiplesOf(3), 0) +
+         boost::accumulate(MultiplesOf(5), 0) -
+         boost::accumulate(MultiplesOf(3 * 5), 0);
 }
 
 struct Result {
@@ -124,29 +110,24 @@ struct Result {
 // Otherwise, the compiler detects that the functions have no side effects and the benchmarks take zero time.
 // Calling through a function pointer forces the compiler to execute the code.
 struct Functor {
-    typedef int (*Function)(int limit, int x, int y);
+    typedef int (*Function)();
 
     Functor() {}
-    Functor(Function function, int limit, int x, int y) : function(function), limit(limit), x(x), y(y) {}
+    Functor(Function function) : function(function) {}
 
-    int operator()() const { return function(limit, x, y); }
+    int operator()() const { return function(); }
 
 private:
     Function function;
-    int limit;
-    int x;
-    int y;
 };
 
 #define RUN_EXPERIMENT(functor) results.emplace_back(Result( \
     #functor, \
-    functor(limit, x, y), \
-    Benchmark(Functor(functor, limit, x, y), 1000)))
+    functor(), \
+    Benchmark(Functor(functor), 1000)))
 
 int main()
 {
-  auto const limit = 1000, x = 3, y = 5;
-
   auto results = std::vector<Result>();
 
   RUN_EXPERIMENT(FilterImperative);
--- 01CSharp/Program.cs
+++ 01CSharp/Program.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Diagnostics;
-using Function = System.Func<int, int, int, int>;
 
 namespace _01CSharp
 {
@@ -24,72 +23,70 @@ namespace _01CSharp
             return stopwatch.Elapsed.TotalMilliseconds;
         }
 
-        // Generate [1, limit)
-        // Filter out multiples of x and y
+        // Generate [1, 1000)
+        // Filter out multiples of 3 and 5
         // Returns sum
-        public static int filterImperative(int limit, int x, int y)
+        public static int filterImperative()
         {
             var sum = 0;
-            for (var i = 1; i < limit; ++i)
-                if (i % x == 0 || i % y == 0)
+            for (var i = 1; i < 1000; ++i)
+                if (i % 3 == 0 || i % 5 == 0)
                     sum += i;
             return sum;
         }
 
-        // Sum multiples of x
-        // Sum multiples of y
-        // Subtract multiples of x * y to eliminate duplicates
-        public static int generateMultiplesImperative(int limit, int x, int y)
+        // Sum multiples of 3
+        // Sum multiples of 5
+        // Subtract multiples of 3 * 5 to eliminate duplicates
+        public static int generateMultiplesImperative()
         {
             var sum = 0;
-            for (var i = x; i < limit; i += x) sum += i;
-            for (var i = y; i < limit; i += y) sum += i;
-
-            var product = x * y;
-            for (var i = product; i < limit; i += product) sum -= i;
+            for (var i = 3; i < 1000; i += 3) sum += i;
+            for (var i = 5; i < 1000; i += 5) sum += i;
+            for (var i = 15; i < 1000; i += 15) sum -= i;
             return sum;
         }
 
-        // Compute the sum of [x, limit) step x using triangular numbers
-        public static int sumOfMultiplesOf(int x, int limit)
+        // Compute the sum of [x, 1000) step x using triangular numbers
+        public static int sumOfMultiplesOf(int x)
         {
-            var p = (limit - 1) / x;
+            var p = (1000 - 1) / x;
             return x * p * (p + 1) / 2;
         }
 
-        // Sum multiples of x
-        // Sum multiples of y
-        // Subtract multiples of x * y to eliminate duplicates
-        public static int sumOfMultiples(int limit, int x, int y)
+        // Sum multiples of 3
+        // Sum multiples of 5
+        // Subtract multiples of 3 * 5 to eliminate duplicates
+        public static int sumOfMultiples()
         {
-            return sumOfMultiplesOf(x, limit) + sumOfMultiplesOf(y, limit) - sumOfMultiplesOf(x * y, limit);
+            return sumOfMultiplesOf(3) + sumOfMultiplesOf(5) - sumOfMultiplesOf(15);
         }
 
-        // Generate [1, limit)
-        // Filter out multiples of x and y
+        // Generate [1, 1000)
+        // Filter out multiples of 3 and 5
         // Returns sum
-        public static int filterFunctional(int limit, int x, int y)
+        public static int filterFunctional()
         {
-            return Enumerable.Range(1, limit - 1).Where(i => i % x == 0 || i % y == 0).Sum();
+            return Enumerable.Range(1, 1000 - 1).Where(i => i % 3 == 0 || i % 5 == 0).Sum();
         }
 
-        // Generate multiples of x up to but not including limit
-        public static IEnumerable<int> multiplesOf(int x, int limit)
+        // Generate multiples of x up to but not including 1000
+        public static IEnumerable<int> multiplesOf(int x)
         {
-            for (var i = x; i < limit; i += x) yield return i;
+            for (var i = x; i < 1000; i += x) yield return i;
         }
 
-        // Sum multiples of x
-        // Sum multiples of y
-        // Subtract multiples of x * y to eliminate duplicates
-        public static int generateMultiplesFunctional(int limit, int x, int y)
+        // Sum multiples of 3
+        // Sum multiples of 5
+        // Subtract multiples of 3 * 5 to eliminate duplicates
+        public static int generateMultiplesFunctional()
         {
-            return multiplesOf(x, limit).Sum() + multiplesOf(y, limit).Sum() - multiplesOf(x * y, limit).Sum();
+            return multiplesOf(3).Sum() + multiplesOf(5).Sum() - multiplesOf(3 * 5).Sum();
         }
 
         static void Main(string[] args)
         {
-            var experiments = new Function[] {
+            var experiments = new Func<int>[] {
                 filterImperative,
                 generateMultiplesImperative,
                 sumOfMultiples,
@@ -97,9 +94,8 @@ namespace _01CSharp
                 generateMultiplesFunctional,
             };
 
-            const int limit = 1000, x = 3, y = 5;
             foreach (var result in experiments
-                .Select(func => new { result = func(limit, x, y), time = benchmark(() => func(limit, x, y), 1000), name = func.Method.Name })
+                .Select(func => new { result = func(), time = benchmark(() => func(), 1000), name = func.Method.Name })
                 .OrderBy(tuple => tuple.time))
                 Console.WriteLine("{0,-30} {1} {2:00.0000}", result.name, result.result, result.time);
         }
